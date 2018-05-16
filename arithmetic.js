b2570bdae8d7c6888c558146f3d7a32d9ae53e18
kaiwells <37965838+kaiwells@users.noreply.github.com>
2018-05-15 21:49:15 -0700

    added subtraction
    also added horizontal/vertical alignment

--- arithmetic.js
+++ arithmetic.js
@@ -7,16 +7,35 @@ var ADDITION = 0;
 var SUBTRACTION = 1;
 var MULTIPLICATION = 2;
 var DIVISION = 3;
+var HORIZONTAL = 0;
+var VERTICAL = 1;
 var number1 = Math.floor(Math.random() * 10);
 var number2 = Math.floor(Math.random() * 10);
 var arithmeticFunction = ADDITION;
+var alignment = HORIZONTAL;
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	if(IMAGES_LOADED === 1){
-		ctx.font = "15px Arial";
-		if (arithmeticFunction === 0) {
-			ctx.fillText(number1 + " + " + number2 + "=",10,30);
+		ctx.font = "45px Arial";
+		if (alignment === HORIZONTAL) {
+			if (arithmeticFunction === ADDITION) {
+				ctx.fillText(number1 + " + " + number2 + " =",130,200);
+			} else if (arithmeticFunction === SUBTRACTION) {
+				ctx.fillText(number1 + " - " + number2 + " =",130,200);
+			}
+		} else {
+			if (arithmeticFunction === ADDITION) {
+				ctx.fillText(number1,190,160);
+				ctx.fillText(number2,190,200);
+				ctx.fillText("+",130,200);
+				ctx.fillText("____",130,200);
+			} else if (arithmeticFunction === SUBTRACTION) {
+				ctx.fillText(number1,190,160);
+				ctx.fillText(number2,190,200);
+				ctx.fillText("-",130,200);
+				ctx.fillText("____",130,200);
+			}
 		}
 	}
 }
@@ -42,15 +61,37 @@ button.addEventListener ("click", function() {
 	guess = guess.toLowerCase("answer");
 	guess = guess.trim(guess);
 	//var fail = document.getElementById("fail");
-	if(parseInt(guess)===number1 + number2) {
-		//var cry = pokemonData[chosenPokemon].cry;
-		//cry.load();
-		//cry.play(); 
-		alert("correct");
-	} else {
-		alert("flail");
+	if (arithmeticFunction === ADDITION) {
+		if(parseInt(guess)===number1 + number2) {
+			alert("correct");
+			if (alignment === HORIZONTAL) {
+				alignment = VERTICAL;
+			} else {
+				alignment = HORIZONTAL;
+			}
+			number1 = Math.floor(Math.random() * 10);
+			number2 = Math.floor(Math.random() * 10);
+			arithmeticFunction = Math.floor(Math.random() * 2);
+			document.getElementById("answer").value = "";
+		} else {
+			alert("flail");
+		}
+	} else if (arithmeticFunction === SUBTRACTION) {
+		if(parseInt(guess)===number1 - number2) {
+			alert("correct");
+			if (alignment === HORIZONTAL) {
+				alignment = VERTICAL;
+			} else {
+				alignment = HORIZONTAL;
+			}
+			number1 = Math.floor(Math.random() * 10);
+			number2 = Math.floor(Math.random() * 10);
+			arithmeticFunction = Math.floor(Math.random() * 2);
+			document.getElementById("answer").value = "";
+		} else {
+			alert("flail");
+		}
 	}
-	  
 });
 
 document.getElementById("answer")
